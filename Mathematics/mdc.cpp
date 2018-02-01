d980cfbcedef16e70fee410a18017d95017f17c7
MateusRangelCC <mateus.cc.contato@gmail.com>
2018-02-01 16:58:03 -0300

    add mdc

---
+++ Mathematics/mdc.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int mdc(int x, int y) {
+
+    if (x < y) {
+        int aux = y;
+        y = x;
+        x = aux;
+    }
+
+    //Se x for múltiplo de y, mds(x,y) = y
+    if ((x%y) == 0) return y;
+
+    //se não, recursividade!
+    return mdc(y, x%y);
+}
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    cout << mdc(x, y) << endl;
+}
