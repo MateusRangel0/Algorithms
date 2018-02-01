066b90bf4fde97563f5bc6546d09e8e29eb46694
MateusRangelCC <mateus.cc.contato@gmail.com>
2018-02-01 19:20:55 -0300

    add crivo

---
+++ Mathematics/crivo.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define MAXN 100
+
+using namespace std;
+
+bitset<MAXN> primes;
+
+void sieve() {
+    primes.set();
+    primes[0] = primes[1] = 0;
+
+    for (long long int i = 2; i < MAXN; i++) {
+        if (primes[i]) {
+            for (long long int j = i*i; j < MAXN; j+= u) primes[j] = 0;
+                //printf("lld ", i);
+        }
+    }
+}
