5af62dcbed55ad160d469f02bbad326177b4e34b
MateusRangelCC <mateus.cc.contato@gmail.com>
2018-02-03 17:33:57 -0300

    add BFS

---
+++ Graph/BFS.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define MAXN 1000007
+
+using namespace std;
+
+vector<int> grafo[MAXN];
+bitset<MAXN> vis;
+int dist[MAXN];
+
+void bfs(int no) {
+    queue<int> q;
+
+    vis[no] = true;
+    dist[no] = 0;
+    while(!q.empty()) {
+        int top = q.front();
+        q.pop();
+
+        for(int i = 0; i < grafo[top].size(); i++) {
+            if(!vis[grafo[top][i]]) {
+                dist[grafo[top][i]] = dist[top] + 1;
+                vis[grafo[top][i]] = true;
+                q.push(grafo[top][i]);
+            }
+        }
+    }
+}
