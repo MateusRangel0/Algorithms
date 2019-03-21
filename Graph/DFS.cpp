4e3ce0f0c57bd731e016cee652fb9cb64a6620b3
Mateus Rangel <mateus.cc.contato@gmail.com>
2019-03-21 13:11:45 -0300

    Update DFS.cpp

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -9,7 +9,7 @@ bitset<MAXN> vis;
 void dfs(int no) {
     vis[no] = true;
     for(int i = 0; i < grafo[no].size(); i++) {
-        if(!vis[grafo][i]) {
+        if(!vis[grafo[node][i]]) {
             dfs(grafo[no][i]);
         }
     }
